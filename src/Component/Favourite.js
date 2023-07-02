2fd2cd42502c3d5c2b82fe597445927870ae43e4
Ridowan-sajid <sajidridowan7@gmail.com>
2023-07-02 21:18:20 +0600

    Change the button

--- src/Component/Favourite.js
+++ src/Component/Favourite.js
@@ -3,17 +3,22 @@ import React from "react";
 export default function Favourite({ favorite, removeToFav }) {
   console.log(favorite);
   return (
-    <div>
+    <>
       <h2>Favorite List</h2>
-      {favorite &&
-        favorite.map((m) => (
-          <div class="d-inline" key={m.imdbID}>
-            <img src={m.Poster} alt="Ops something wrong" />
-            <button className="btn btn-danger" onClick={() => removeToFav(m)}>
-              Remove
-            </button>
-          </div>
-        ))}
-    </div>
+      <div class="d-flex flex-wrap">
+        {favorite &&
+          favorite.map((m) => (
+            <div key={m.imdbID} className="m-1">
+              <img src={m.Poster} alt="Ops something wrong" />
+              <button
+                className="btn btn-danger w-100"
+                onClick={() => removeToFav(m)}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
--- src/Component/MovieList.js
+++ src/Component/MovieList.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function MovieList({ movies, addToFav }) {
   return (
-    <div>
+    <div class="d-flex flex-wrap">
       {movies.map((m) => (
-        <div class="d-inline" key={m.imdbID}>
+        <div key={m.imdbID} className="m-1">
           <img src={m.Poster} alt="Ops something wrong" />
-          <button className="btn btn-dark" onClick={() => addToFav(m)}>
+          <button className="btn btn-success w-100" onClick={() => addToFav(m)}>
             Add
           </button>
         </div>
